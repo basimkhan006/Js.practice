c434dad43c4dc9ae131145a6f4a0f4eab680f775
basimkhan <basimkhurram0@gmail.com>
2026-06-11 20:46:15 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,49 @@
+// function register(name, age, payment) {
+//     this.name = name;
+//     this.age = age;
+//     this.payment = payment;
+// }
+
+// function handleForm() {
+//     const name = prompt("enter name")
+//     const age = prompt("enter age")
+//     const payment = prompt("enter payment")
+
+//     const record = new register(name,age,payment)
+
+//     console.log(record)
+// }
+
+
+// function NewMobile(model, storage, color){
+//     this.model = model;
+//     this.storage = storage;
+//     this.color = color;
+// }
+
+// function handleForm(){
+//     const model = prompt("enter your model")
+//     const storage  = prompt("enter your storage")
+//     const color = prompt("enter your color")
+
+//     const record = new NewMobile(model, storage, color)
+
+//     console.log(record)
+// }
+
+
+
+// let name = "basim"
+// let age = 20
+
+// // document.write("my name is " + name ," ","my age is " + age)    
+// document.write(`my name is ${name} and my age is ${age}`)    
+
+
+// let num = prompt("enter table num")
+// let num1 = prompt("enter second num")
+
+
+// for (i = num1; i <= num1; i++) {
+//     document.writeln(`${num} x ${i} = ${num * num1}` + "<br>")
+// }
