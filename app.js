f2b2d94c573ffa91ff3ac92b33301e81f4a4c202
basimkhan <basimkhurram0@gmail.com>
2026-06-16 20:26:58 +0500

    Update app.js

--- app.js
+++ app.js
@@ -54,3 +54,55 @@
 // p.style.fontSize = "60px"
 // p.style.height = "450px"
 // p.style.fontWeight = "bold"
+
+
+// const div = document.createElement('div')
+// div.style.width = "50%"
+// div.style.height = "200px"
+// div.style.border = "2px solid black"
+
+// div.appendChild(para)
+
+// let mainDiv = document.getElementById('main');
+// mainDiv.style.backgroundColor = "green"
+
+
+const navbar = document.createElement('nav')
+navbar.style.width = "100%"
+navbar.style.height = "100px"
+// navbar.style.backgroundColor = "black"
+navbar.style.border = "2px solid yellow"
+navbar.style.display = "flex";
+navbar.style.alignItems = "center";
+navbar.style.justifyContent = "space-evenly";
+navbar.setAttribute('class','navbar')
+
+document.body.appendChild(navbar)
+
+const heading = document.createElement('h1');
+heading.innerHTML = "Nav Bar";
+heading.style.color = "white";
+heading.setAttribute('class','heading')
+
+
+const home = document.createElement('a');
+home.innerText = "Home";
+home.style.color = "white";
+home.setAttribute('class','home')
+
+const about = document.createElement('a');
+about.innerText = "About";
+about.style.color = "white";
+about.setAttribute('class','about')
+
+const contact = document.createElement('a');
+contact.innerText = "Contact";
+contact.style.color = "white";
+contact.setAttribute('class','contact')
+
+navbar.appendChild(heading);
+navbar.appendChild(home);
+navbar.appendChild(about);
+navbar.appendChild(contact);
+
+document.body.appendChild(navbar);
